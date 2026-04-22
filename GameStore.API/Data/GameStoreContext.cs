e688b6367e5e2b339a0d8f506020bfa95d858a1d
Miguel Gemio Quispe <Miguel.Gemio@jalasoft.com>
2026-04-22 18:05:39 -0400

    Add SQL Liste EF connection

---
+++ GameStore.API/Data/GameStoreContext.cs
@@ -0,0 +1,13 @@
+using GameStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.API.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+    // After installing ef, and EntityFramework.Design
+    // Run Command: dotnet ef migrations add InitialCreate --output-dir Data/Migrations
+    // dotnet ef database update
+}
--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,8 +1,13 @@
+using GameStore.API.Data;
 using GameStore.API.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 
 var app = builder.Build();
 
